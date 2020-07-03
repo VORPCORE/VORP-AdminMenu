3a9cdf338dd6f37aa469dedee000ebcff1244fad
Esteban <qwerty62000@gmail.com>
2020-07-04 01:51:33 +0200

    update banneds sql script

--- VORP_AdminMenu[Client-Server]/build/VORP_AdminMenu/banneds.sql
+++ VORP_AdminMenu[Client-Server]/build/VORP_AdminMenu/banneds.sql
@@ -1,36 +1,25 @@
-/*
- Navicat Premium Data Transfer
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Source Server         : Casa
- Source Server Type    : MySQL
- Source Server Version : 100411
- Source Host           : localhost:3306
- Source Schema         : vorp
 
- Target Server Type    : MySQL
- Target Server Version : 100411
- File Encoding         : 65001
+CREATE TABLE `banneds` (
+  `b_id` int(11) NOT NULL,
+  `b_steam` varchar(100) NOT NULL,
+  `b_license` varchar(255) DEFAULT NULL,
+  `b_discord` varchar(100) DEFAULT NULL,
+  `b_reason` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `b_banned` varchar(100) NOT NULL,
+  `b_unban` varchar(100) NOT NULL,
+  `b_permanent` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
- Date: 27/06/2020 19:08:15
-*/
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+ALTER TABLE `banneds`
+  ADD PRIMARY KEY (`b_id`) USING BTREE;
 
--- ----------------------------
--- Table structure for banneds
--- ----------------------------
-DROP TABLE IF EXISTS `banneds`;
-CREATE TABLE `banneds`  (
-  `b_id` int(11) NOT NULL AUTO_INCREMENT,
-  `b_steam` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `b_license` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `b_discord` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `b_reason` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL,
-  `b_banned` datetime(0) NOT NULL,
-  `b_unban` datetime(0) NOT NULL,
-  `b_permanent` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`b_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-SET FOREIGN_KEY_CHECKS = 1;
+ALTER TABLE `banneds`
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
