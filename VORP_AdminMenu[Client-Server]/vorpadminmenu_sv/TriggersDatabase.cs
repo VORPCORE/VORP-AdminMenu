b5e1586d3e72833874d9187689a00bd75c9e48f4
Esteban <qwerty62000@gmail.com>
2020-07-05 02:47:39 +0200

    itemName/weaponName/ammoName input Control

--- VORP_AdminMenu[Client-Server]/vorpadminmenu_sv/TriggersDatabase.cs
+++ VORP_AdminMenu[Client-Server]/vorpadminmenu_sv/TriggersDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,76 +29,203 @@ namespace vorpadminmenu_sv
 
         private void AdminAddMoney([FromSource]Player source, List<object> args)
         {
-            int id = int.Parse(args[0].ToString());
-            int type = int.Parse(args[1].ToString());
-            double quantity = double.Parse(args[2].ToString());
-            if (type == 2)
+            bool idC = int.TryParse(args[0].ToString(),out int id);
+            bool typeC = int.TryParse(args[1].ToString(), out int type);
+            
+            if (idC && typeC)
             {
-                Debug.WriteLine("entra");
-                int intQuantity = (int)Math.Ceiling(quantity);
-                Debug.WriteLine(intQuantity.ToString());
-
-                TriggerEvent("vorp:addMoney", id, type, intQuantity);
+                if (type == 2)
+                {
+                    bool quantityC = double.TryParse(args[2].ToString(), out double quantity);
+                    if (quantityC)
+                    {
+                        int intQuantity = (int)Math.Ceiling(quantity);
+                        TriggerEvent("vorp:addMoney", id, type, intQuantity);
+                    } 
+                    else
+                    {
+                        bool quantityCInt = int.TryParse(args[2].ToString(), out int quantityInt);
+                        if (quantityCInt)
+                        {
+                            TriggerEvent("vorp:addMoney", id, type, quantityInt);
+                        }
+                        else
+                        {
+                            Debug.WriteLine("Bad syntax");
+                        }
+                    }
+                }
+                else if (type == 0 || type == 1)
+                {
+                    bool quantityC = double.TryParse(args[2].ToString(), out double quantity);
+                    if (quantityC)
+                    {
+                        TriggerEvent("vorp:addMoney", id, type, quantity);
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Bad syntax");
+                }
             }
             else
             {
-                TriggerEvent("vorp:addMoney", id, type, quantity);
+                Debug.WriteLine("Bad syntax");
             }
         }
 
         private void AdminRemoveMoney([FromSource]Player source, List<object> args)
         {
-            int id = int.Parse(args[0].ToString());
-            int type = int.Parse(args[1].ToString());
-            double quantity = double.Parse(args[2].ToString());
-            if (type == 2)
+            bool idC = int.TryParse(args[0].ToString(), out int id);
+            bool typeC = int.TryParse(args[1].ToString(), out int type);
+
+            if (idC && typeC)
             {
-                TriggerEvent("vorp:removeMoney", id, type, (int)quantity);
-            } else
+                if (type == 2)
+                {
+                    bool quantityC = double.TryParse(args[2].ToString(), out double quantity);
+                    if (quantityC)
+                    {
+                        int intQuantity = (int)Math.Ceiling(quantity);
+                        TriggerEvent("vorp:removeMoney", id, type, intQuantity);
+                    }
+                    else
+                    {
+                        bool quantityCInt = int.TryParse(args[2].ToString(), out int quantityInt);
+                        if (quantityCInt)
+                        {
+                            TriggerEvent("vorp:removeMoney", id, type, quantityInt);
+                        }
+                        else
+                        {
+                            Debug.WriteLine("Bad syntax");
+                        }
+                    }
+                }
+                else if (type == 0 || type == 1)
+                {
+                    bool quantityC = double.TryParse(args[2].ToString(), out double quantity);
+                    if (quantityC)
+                    {
+                        TriggerEvent("vorp:removeMoney", id, type, quantity);
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Bad syntax");
+                }
+            }
+            else
             {
-                TriggerEvent("vorp:removeMoney", id, type, quantity);
+                Debug.WriteLine("Bad syntax");
             }
         }
 
         private void AdminAddXp([FromSource]Player source, List<object> args)
         {
-            int id = int.Parse(args[0].ToString());
-            int quantity = int.Parse(args[1].ToString());
-            TriggerEvent("vorp:addXp", id, quantity);
+            bool idC = int.TryParse(args[0].ToString(), out int id);
+            bool quantityC = int.TryParse(args[1].ToString(), out int quantity);
+            if (idC && quantityC)
+            {
+                TriggerEvent("vorp:addXp", id, quantity);
+            }
+            else
+            {
+                Debug.WriteLine("Bad syntax");
+            }
         }
 
         private void AdminRemoveXp([FromSource]Player source, List<object> args)
         {
-            int id = int.Parse(args[0].ToString());
-            int quantity = int.Parse(args[1].ToString());
-            TriggerEvent("vorp:removeXp", id, quantity);
+            bool idC = int.TryParse(args[0].ToString(), out int id);
+            bool quantityC = int.TryParse(args[1].ToString(), out int quantity);
+            if (idC && quantityC)
+            {
+                TriggerEvent("vorp:removeXp", id, quantity);
+            }
+            else
+            {
+                Debug.WriteLine("Bad syntax");
+            }
         }
 
         private void AdminAddItem([FromSource]Player source, List<object> args)
         {
-            int idPlayer = int.Parse(args[0].ToString());
+            bool idC = int.TryParse(args[0].ToString(), out int id);
             string item = args[1].ToString();
-            string quantity = args[2].ToString();
-            TriggerEvent("vorpCore:addItem", idPlayer, item, quantity);
+            bool quantityC = int.TryParse(args[2].ToString(), out int quantity);
+            
+           
+            if (idC && quantityC)
+            {
+                Exports["ghmattimysql"].execute("SELECT * FROM items WHERE item=(?)", new[] { item }, new Action<dynamic>((result) =>
+                {
+                    if (result.Count != 0)
+                    {
+                        TriggerEvent("vorpCore:addItem", id, item, quantity);
+                    } 
+                    else
+                    {
+                        Debug.WriteLine(item + " doesn't exist in db");
+                    }
+
+                }));
+                
+            }
+            else
+            {
+                Debug.WriteLine("Bad syntax");
+            }
         }
 
         private void AdminDelItem([FromSource]Player source, List<object> args)
         {
-            int idPlayer = int.Parse(args[0].ToString());
+            bool idC = int.TryParse(args[0].ToString(), out int id);
             string item = args[1].ToString();
-            string quantity = args[2].ToString();
-            TriggerEvent("vorpCore:subItem", idPlayer, item, quantity);
+            bool quantityC = int.TryParse(args[2].ToString(), out int quantity);
+            if (idC && quantityC)
+            {
+                TriggerEvent("vorpCore:subItem", id, item, quantity);
+            }
+            else
+            {
+                Debug.WriteLine("Bad syntax");
+            }
         }
 
         private void AdminAddWeapon([FromSource]Player source, List<object> args)
         {
-            int idPlayer = int.Parse(args[0].ToString());
+            bool idC = int.TryParse(args[0].ToString(), out int id);
             string item = args[1].ToString();
+            bool wC = false;
+            foreach (string w in Utils.WeaponList.weapons)
+            {
+                if(w == item)
+                {
+                    wC = true;
+                }
+            }
+
             string ammo = args[2].ToString();
-            int quantity = int.Parse(args[3].ToString());
-            Dictionary<string, int> ammoaux = new Dictionary<string, int>();
-            ammoaux.Add(ammo, quantity);
-            TriggerEvent("vorpCore:registerWeapon", idPlayer, item, ammoaux, ammoaux);
+            bool aC = false;
+            foreach (string a in Utils.AmmoList.ammo)
+            {
+                if (a == ammo)
+                {
+                    aC = true;
+                }
+            }
+            bool quantityC = int.TryParse(args[2].ToString(), out int quantity);
+            if(idC && wC && aC && quantityC)
+            {
+                Dictionary<string, int> ammoaux = new Dictionary<string, int>();
+                ammoaux.Add(ammo, quantity);
+                TriggerEvent("vorpCore:registerWeapon", id, item, ammoaux, ammoaux);
+            }
+            else
+            {
+                Debug.WriteLine("Bad syntax");
+            }
         }
 
         private void GetInventory([FromSource]Player source, List<object> args)
