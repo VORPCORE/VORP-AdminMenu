9b9dd9e5bcbacfbb55958e50bca011968d336e2a
Artzalez <devbiitz2@gmail.com>
2020-09-24 05:11:38 +0100

    I go to sleep
    nah some shit

--- VORP_AdminMenu[Client-Server]/vorpadminmenu_sv/PlayerBanned.cs
+++ VORP_AdminMenu[Client-Server]/vorpadminmenu_sv/PlayerBanned.cs
@@ -9,45 +9,45 @@ namespace vorpadminmenu_sv
 {
     public class PlayerBanned : BaseScript
     {
-        int id;
-        string steam;
-        string license;
-        string discord;
-        DateTime banned;
-        DateTime unban;
-        bool permanent;
-        string reason;
+        //int id;
+        //string steam;
+        //string license;
+        //string discord;
+        //DateTime banned;
+        //DateTime unban;
+        //bool permanent;
+        //string reason;
 
-        public PlayerBanned()
-        {
+        //public PlayerBanned()
+        //{
 
-        }
+        //}
 
-        public PlayerBanned(int id, string steam, string license, string discord, DateTime banned, DateTime unban, bool permanent, string reason = "")
-        {
-            this.id = id;
-            this.steam = steam;
-            this.license = license;
-            this.discord = discord;
-            this.banned = banned;
-            this.unban = unban;
-            this.permanent = permanent;
-            this.reason = reason;
-        }
+        //public PlayerBanned(int id, string steam, string license, string discord, DateTime banned, DateTime unban, bool permanent, string reason = "")
+        //{
+        //    this.id = id;
+        //    this.steam = steam;
+        //    this.license = license;
+        //    this.discord = discord;
+        //    this.banned = banned;
+        //    this.unban = unban;
+        //    this.permanent = permanent;
+        //    this.reason = reason;
+        //}
 
-        public int Id { get => id; set => id = value; }
-        public string Steam { get => steam; set => steam = value; }
-        public string License { get => license; set => license = value; }
-        public string Discord { get => discord; set => discord = value; }
-        public DateTime Banned { get => banned; set => banned = value; }
-        public DateTime Unban { get => unban; set => unban = value; }
-        public bool Permanent { get => permanent; set => permanent = value; }
-        public string Reason { get => reason; set => reason = value; }
+        //public int Id { get => id; set => id = value; }
+        //public string Steam { get => steam; set => steam = value; }
+        //public string License { get => license; set => license = value; }
+        //public string Discord { get => discord; set => discord = value; }
+        //public DateTime Banned { get => banned; set => banned = value; }
+        //public DateTime Unban { get => unban; set => unban = value; }
+        //public bool Permanent { get => permanent; set => permanent = value; }
+        //public string Reason { get => reason; set => reason = value; }
 
-        public void DeleteInDB()
-        {
-            Exports["ghmattimysql"].execute($"DELETE FROM BANNEDS WHERE b_id=?", new[] { id });
-        }
-
+        //public void DeleteInDB()
+        //{
+        //    Exports["ghmattimysql"].execute($"DELETE FROM BANNEDS WHERE b_id=?", new[] { id });
+        //}
+        //Trash class now core have ban option and warnings.
     }
 }
